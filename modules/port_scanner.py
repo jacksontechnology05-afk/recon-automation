e1fd034f160fb6b47ed0e2b1a347781c9d7a5a85
Jackson Nnkemdilim Godwin Ojebor <jacksontechnology05@gmail.com>
2026-05-20 10:49:49 +0100

    Update port_scanner.py

--- modules/port_scanner.py
+++ modules/port_scanner.py
@@ -1,1 +1,20 @@
+import socket
 
+target = input("Enter target IP or domain: ")
+
+ports = [20, 21, 22, 23, 25, 53, 80, 110, 135, 139, 143, 443, 445, 3306, 3389, 8080]
+
+print(f"\n[+] Scanning {target}...\n")
+
+for port in ports:
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.settimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"[+] Port {port} is OPEN")
+
+    s.close()
