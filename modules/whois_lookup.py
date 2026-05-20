d87780c53045df79dc890ba26ce7baea7c64d333
Jackson Nnkemdilim Godwin Ojebor <jacksontechnology05@gmail.com>
2026-05-20 11:14:16 +0100

    Update whois_lookup.py

--- modules/whois_lookup.py
+++ modules/whois_lookup.py
@@ -1,7 +1,16 @@
 import whois
+import json
 
 domain = input("Enter domain: ")
 
-info = whois.whois(domain)
+try:
 
-print(info)
+    info = whois.whois(domain)
+
+    print("\n[+] WHOIS Information:\n")
+
+    print(json.dumps(info, indent=4, default=str))
+
+except Exception as e:
+
+    print(f"\n[-] Error: {e}")
