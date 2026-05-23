871c7832a75906b04ea436d8350a821b134af63d
Jackson Nnkemdilim Godwin Ojebor <jacksontechnology05@gmail.com>
2026-05-23 22:24:47 +0100

    Create cve_fingerprint.py

---
+++ modules/cve_fingerprint.py
@@ -0,0 +1,19 @@
+service = input("Enter service or technology name: ")
+
+print("\n===================================================")
+print("            CVE FINGERPRINTING")
+print("===================================================\n")
+
+print(f"[+] Searching for known issues related to: {service}")
+
+print("""
+Example:
+Apache 2.4
+OpenSSH
+WordPress
+Joomla
+PHP
+
+Recommendation:
+Validate detected technologies against known CVEs.
+""")
