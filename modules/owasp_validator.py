11f3aca775b484b661f39d6253e9cd204cf75917
Jackson Nnkemdilim Godwin Ojebor <jacksontechnology05@gmail.com>
2026-05-23 22:04:05 +0100

    Create owasp_validator.py

---
+++ modules/owasp_validator.py
@@ -0,0 +1,21 @@
+print("""
+===================================================
+          OWASP TOP 10 VALIDATOR
+===================================================
+
+OWASP Categories:
+
+A01 - Broken Access Control
+A02 - Cryptographic Failures
+A03 - Injection
+A04 - Insecure Design
+A05 - Security Misconfiguration
+A06 - Vulnerable Components
+A07 - Authentication Failures
+A08 - Software Integrity Failures
+A09 - Logging Failures
+A10 - SSRF
+
+Recommendation:
+Validate applications against OWASP Top 10 risks.
+""")
