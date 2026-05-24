23880c1a6cb07cfd49fdf802c09447ec0525689a
Jackson Nnkemdilim Godwin Ojebor <jacksontechnology05@gmail.com>
2026-05-24 05:37:23 +0100

    Create compliance_mapper.py

---
+++ modules/compliance_mapper.py
@@ -0,0 +1,23 @@
+print("""
+===================================================
+           COMPLIANCE MAPPER
+===================================================
+
+Supported Frameworks:
+
+- ISO 27001
+- NIST Cybersecurity Framework
+- PCI-DSS
+
+Example Mapping:
+
+Missing CSP Header:
+ISO 27001 A.8.28
+NIST PR.DS-6
+PCI-DSS Requirement 6.5
+
+Weak TLS:
+ISO 27001 A.8.24
+NIST PR.DS-2
+PCI-DSS Requirement 4.1
+""")
